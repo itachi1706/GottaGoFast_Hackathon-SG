a489f1ecda7a13ec66993c7da9f3292545d05123
itachi1706 <kennethsohyq@hotmail.com>
2015-07-26 02:26:19 +0800

    Added a verification method to prevent theft of code heheheh :P

--- app/src/main/java/com/itachi1706/hackathonsg/MainSettings.java
+++ app/src/main/java/com/itachi1706/hackathonsg/MainSettings.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -18,9 +19,12 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.Date;
 import java.util.List;
@@ -93,6 +97,62 @@ public class MainSettings extends AppCompatActivity {
             long dbBus = sp.getLong("lastQueried", -1);
             updateSummaryDBBus(timeDBUpdateBus, dbBus);
 
+            verPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    if (!isActive) {
+                        if (count == 10) {
+                            count = 0;
+                            startEgg();
+                            Snackbar.make(getActivity().findViewById(android.R.id.content), "This app is verified to be made by GottaGoFast for Hackathon@SG 2015", Snackbar.LENGTH_LONG)
+                                    .setAction("Okay", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Toast.makeText(getActivity(), "Aww okay... :(", Toast.LENGTH_SHORT).show();
+                                            endEgg();
+                                        }
+                                    }).show();
+                        } else {
+                            switch (count) {
+                                case 5:
+                                    prompt(5);
+                                    break;
+                                case 6:
+                                    prompt(4);
+                                    break;
+                                case 7:
+                                    prompt(3);
+                                    break;
+                                case 8:
+                                    prompt(2);
+                                    break;
+                                case 9:
+                                    prompt(1);
+                                    break;
+                            }
+                        }
+                        count++;
+                    }
+                    return false;
+                }
+            });
+
+        }
+
+        MediaPlayer mp;
+        int count = 0;
+        Toast toasty;
+        boolean isActive = false;
+
+        private void prompt(int left){
+            if (toasty != null){
+                toasty.cancel();
+            }
+            if (left > 1)
+                toasty = Toast.makeText(getActivity(), left + " more clicks for verification!", Toast.LENGTH_SHORT);
+            else
+                toasty = Toast.makeText(getActivity(), left + " more click for verification!", Toast.LENGTH_SHORT);
+            toasty.show();
         }
 
         private void updateSummaryDBBus(Preference timeDBUpdateBus, long dbBus){
@@ -102,6 +162,39 @@ public class MainSettings extends AppCompatActivity {
             }
             Date date = new Date(dbBus);
             timeDBUpdateBus.setSummary(date.toString());
+        }
+
+        @Override
+        public void onResume(){
+            super.onResume();
+            count = 0;
+        }
+
+        @Override
+        public void onPause(){
+            super.onPause();
+            endEgg();
+        }
+
+        private void startEgg(){
+            if (!isActive) {
+                mp = MediaPlayer.create(getActivity(), R.raw.settings_fun);
+                mp.start();
+                isActive = true;
+            }
+        }
+
+        private void endEgg(){
+            count = 0;
+            isActive = false;
+            if (mp != null){
+                if (mp.isPlaying()){
+                    mp.stop();
+                    mp.reset();
+                }
+                mp.release();
+                mp = null;
+            }
         }
     }
 }
